9ebc7fff4636bc135343f407dfbf661c0a0b320e
joshebrani <joshuaebrani@gmail.com>
2021-01-22 16:01:01 -0500

    Created methods for anagrams at various complexities

--- anagrams.rb
+++ anagrams.rb
@@ -1,3 +1,4 @@
+# O(n!)
 def first_anagram?(str_1, str_2)
     angrams_1 = str_1.chars.permutation.to_a
     angrams_1.any? { |arr| arr.join("") == str_2}
@@ -6,6 +7,7 @@ end
 p first_anagram?("gizmo", "sally")    #=> false
 p first_anagram?("elvis", "lives")    #=> true
 
+# O(n)
 def second_anagram?(str_1,str_2)
     return false  if str_1.length != str_2.length 
     (0...str_1.length).each do |i|
@@ -20,8 +22,37 @@ def second_anagram?(str_1,str_2)
     # find index in string_2(use.find_index) and delete index if found  
     # if str_2.empty? return true
 
-
 end
 
 p second_anagram?("gizmo", "sally")    #=> false
 p second_anagram?("elvis", "lives")    #=> true
+
+
+# O(n log n)
+def third_anagram?(str_1, str_2)
+    arr_1 = str_1.chars
+    arr_2 = str_2.chars
+
+    arr_1.sort == arr_2.sort
+end
+
+
+p third_anagram?("gizmo", "sally")    #=> false
+p third_anagram?("elvis", "lives")    #=> true
+
+
+# O(n)
+def fourth_anagram?(str_1, str_2)
+    hash1 = Hash.new(0)
+    hash2 = Hash.new(0)
+
+    str_1.each_char { |char| hash1[char] += 1}
+    str_2.each_char { |char| hash2[char] += 1}
+
+    hash1 == hash2
+
+end
+
+
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")    #=> true
